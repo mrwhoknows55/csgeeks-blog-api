5c3fec7d731642472adabee84de5081b8a33e7cc
mrwhoknows55 <avdhuttanugade55@gmail.com>
2020-09-06 21:31:39 +0530

    patch: delete route ✔

--- src/api/ArticleResource.kt
+++ src/api/ArticleResource.kt
@@ -10,169 +10,170 @@ import io.ktor.routing.*
 
 fun Route.article(articleService: ArticleService) {
 
-    get("/") {
-        call.respond(HttpStatusCode.OK, "Hello, this api is working! \uD83C\uDF89")
-    }
+	get("/") {
+		call.respond(HttpStatusCode.OK, "Hello, this api is working! \uD83C\uDF89")
+	}
 
-    route("/article") {
+	route("/article") {
 
-        get("/{id}") {
-            val param = call.parameters["id"]
+		get("/{id}") {
+			val param = call.parameters["id"]
 
-            try {
-                val id = Integer.parseInt(param)
-                val article = articleService.getArticleById(id)
+			try {
+				val id = Integer.parseInt(param)
+				val article = articleService.getArticleById(id)
 
-                if (article != null) {
-                    call.respond(
-                        HttpStatusCode.OK,
-                        mapOf(
-                            "article" to article,
-                            "success" to true
-                        )
-                    )
-                } else {
-                    call.respond(
-                        HttpStatusCode.NotFound,
-                        mapOf(
-                            "success" to false,
-                            "response" to "Article not found"
-                        )
-                    )
-                }
+				if (article != null) {
+					call.respond(
+							HttpStatusCode.OK,
+							mapOf(
+									"article" to article,
+									"success" to true
+							)
+					)
+				} else {
+					call.respond(
+							HttpStatusCode.NotFound,
+							mapOf(
+									"success" to false,
+									"response" to "Article not found"
+							)
+					)
+				}
 
-            } catch (e: NumberFormatException) {
-                if (param?.toLowerCase().equals("all")) {
-                    val articleResponse = articleService.getAllArticles()
-                    call.respond(
-                        HttpStatusCode.OK,
-                        mapOf(
-                            "articles" to articleResponse,
-                            "success" to true
-                        )
-                    )
-                } else {
-                    call.respond(
-                        HttpStatusCode.BadRequest,
-                        mapOf("success" to false, "response" to "Error!")
-                    )
-                }
-            }
-        }
+			} catch (e: NumberFormatException) {
+				if (param?.toLowerCase().equals("all")) {
+					val articleResponse = articleService.getAllArticles()
+					call.respond(
+							HttpStatusCode.OK,
+							mapOf(
+									"articles" to articleResponse,
+									"success" to true
+							)
+					)
+				} else {
+					call.respond(
+							HttpStatusCode.BadRequest,
+							mapOf("success" to false, "response" to "Error!")
+					)
+				}
+			}
+		}
 
-        post("/") {
-            try {
-                val article = call.receive<Article>()
+		post("/") {
+			try {
+				val article = call.receive<Article>()
 
-                val articleResponse = articleService.saveArticle(article)
+				val articleResponse = articleService.saveArticle(article)
 
-                if (articleResponse != null)
-                    call.respond(
-                        HttpStatusCode.Created,
-                        mapOf(
-                            "success" to true,
-                            "response" to "Article Created Successfully!"
-                        )
-                    )
-                else
-                    call.respond(
-                        HttpStatusCode.BadGateway,
-                        mapOf(
-                            "success" to false,
-                            "response" to "Server Error Try After Some Time"
-                        )
-                    )
+				if (articleResponse != null)
+					call.respond(
+							HttpStatusCode.Created,
+							mapOf(
+									"success" to true,
+									"response" to "Article Created Successfully!"
+							)
+					)
+				else
+					call.respond(
+							HttpStatusCode.BadGateway,
+							mapOf(
+									"success" to false,
+									"response" to "Server Error Try After Some Time"
+							)
+					)
 
-            } catch (e: Exception) {
-                e.printStackTrace()
-                call.respond(
-                    HttpStatusCode.ExpectationFailed,
-                    mapOf(
-                        "success" to false,
-                        "response" to "Error! Please send all fields"
-                    )
-                )
-            }
-        }
+			} catch (e: Exception) {
+				e.printStackTrace()
+				call.respond(
+						HttpStatusCode.ExpectationFailed,
+						mapOf(
+								"success" to false,
+								"response" to "Error! Please send all fields"
+						)
+				)
+			}
+		}
 
-        put("/{id}") {
-            val param = call.parameters["id"]
+		put("/{id}") {
+			val param = call.parameters["id"]
 
-            try {
-                val id = Integer.parseInt(param)
-                val article: Article? = call.receive()
+			try {
+				val id = Integer.parseInt(param)
+				val article: Article? = call.receive()
 
-                if (article != null) {
-                    val articleResponse = articleService.updateArticle(id, article)
+				if (article != null) {
+					val articleResponse = articleService.updateArticle(id, article)
 
-                    if (articleResponse != null)
-                        call.respond(
-                            HttpStatusCode.OK,
-                            mapOf(
-                                "success" to true,
-                                "response" to "Article Updated Successfully!"
-                            )
-                        )
-                    else
-                        call.respond(
-                            HttpStatusCode.BadGateway,
-                            mapOf(
-                                "success" to false,
-                                "response" to "Server Error Try After Some Time"
-                            )
-                        )
-                } else {
-                    call.respond(
-                        HttpStatusCode.NotFound,
-                        mapOf(
-                            "success" to false,
-                            "response" to "Article not found"
-                        )
-                    )
-                }
+					if (articleResponse != null)
+						call.respond(
+								HttpStatusCode.OK,
+								mapOf(
+										"success" to true,
+										"response" to "Article Updated Successfully!"
+								)
+						)
+					else
+						call.respond(
+								HttpStatusCode.BadGateway,
+								mapOf(
+										"success" to false,
+										"response" to "Server Error Try After Some Time"
+								)
+						)
+				} else {
+					call.respond(
+							HttpStatusCode.NotFound,
+							mapOf(
+									"success" to false,
+									"response" to "Article not found"
+							)
+					)
+				}
 
-            } catch (e: NumberFormatException) {
-                call.respond(
-                    HttpStatusCode.BadRequest,
-                    mapOf("success" to false, "response" to "Error!")
-                )
-            }
-        }
-    }
+			} catch (e: NumberFormatException) {
+				call.respond(
+						HttpStatusCode.BadRequest,
+						mapOf("success" to false, "response" to "Error!")
+				)
+			}
+		}
 
-    //  TODO make -> only one or more things can be updatable
-    delete("{id}") {
-        val param = call.parameters["id"]
 
-        try {
-            val id = Integer.parseInt(param)
-            val isDeleted = articleService.deleteArticle(id)
+		//  TODO make -> only one or more things can be updatable
+		delete("{id}") {
+			val param = call.parameters["id"]
 
-            if (isDeleted)
-                call.respond(
-                    HttpStatusCode.OK,
-                    mapOf(
-                        "success" to true,
-                        "response" to "Article Deleted Successfully!"
-                    )
-                )
-            else
-                call.respond(
-                    HttpStatusCode.NotFound,
-                    mapOf(
-                        "success" to false,
-                        "response" to "Article Not Found!"
-                    )
-                )
+			try {
+				val id = Integer.parseInt(param)
+				val isDeleted = articleService.deleteArticle(id)
 
-        } catch (e: NumberFormatException) {
-            call.respond(
-                HttpStatusCode.BadRequest,
-                mapOf(
-                    "success" to false,
-                    "response" to "Error! Enter Correct Id "
-                )
-            )
-        }
-    }
+				if (isDeleted)
+					call.respond(
+							HttpStatusCode.OK,
+							mapOf(
+									"success" to true,
+									"response" to "Article Deleted Successfully!"
+							)
+					)
+				else
+					call.respond(
+							HttpStatusCode.NotFound,
+							mapOf(
+									"success" to false,
+									"response" to "Article Not Found!"
+							)
+					)
+
+			} catch (e: NumberFormatException) {
+				call.respond(
+						HttpStatusCode.BadRequest,
+						mapOf(
+								"success" to false,
+								"response" to "Error! Enter Correct Id "
+						)
+				)
+			}
+		}
+	}
 }
